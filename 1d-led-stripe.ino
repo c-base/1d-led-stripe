194900bb118834f5dd1f48b418ad11558e4ebd4d
coon <coon@c-base.org>
2018-01-01 00:51:12 +0100

    add basic animation

--- 1d-led-stripe.ino
+++ 1d-led-stripe.ino
@@ -18,7 +18,7 @@
 // example for more information on possible values.
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
-int delayval = 500; // delay for half a second
+int delayval = 100; // delay for half a second
 
 void setup() {
   // This is for Trinket 5V 16MHz, you can remove these three lines if you are not using a Trinket
@@ -30,18 +30,52 @@ void setup() {
   pixels.begin(); // This initializes the NeoPixel library.
 }
 
-void loop() {
-
-  // For a set of NeoPixels the first NeoPixel is 0, second is 1, all the way up to the count of pixels minus one.
-
-  for(int i=0;i<NUMPIXELS;i++){
-
-    // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
-    pixels.setPixelColor(i, pixels.Color(0,150,0)); // Moderately bright green color.
-
-    pixels.show(); // This sends the updated pixel color to the hardware.
-
-    delay(delayval); // Delay for a period of time (in milliseconds).
-
+void clearLeds() {
+  const int range = 6;
+  
+  for(int i = 0; i < NUMPIXELS; ++i) {
+    int red = 0;
+    int green = 0;
+    int blue = 0;
+        
+    if(i < range || i >= (NUMPIXELS - range)) {
+      red   = 0;
+      green = 0;
+      blue  = 16;
+    }
+    
+    pixels.setPixelColor(i, pixels.Color(red, green, blue));
   }
 }
+
+enum direction {
+  UP,
+  DOWN
+};
+
+void loop() {
+  const int ledCount = NUMPIXELS;
+  static int ledPos = 0;
+  static int direction = UP;
+  
+  for(int i = 0; i < ledCount; i++){
+    clearLeds();
+    pixels.setPixelColor(ledPos, pixels.Color(0,150,0)); // Moderately bright green color.        
+  }
+
+  pixels.show(); // This sends the updated pixel color to the hardware.
+  delay(delayval); // Delay for a period of time (in milliseconds).
+
+  if(direction == UP)
+    ledPos++;
+  else
+    ledPos--;
+
+
+  if(ledPos == 0)
+    direction = UP;
+  
+  if(ledPos == NUMPIXELS - 1)
+    direction = DOWN;
+}
+
