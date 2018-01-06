d6ea9fcd199770a8aea1dfb8f17001c40c3b38c6
coon <coon@mailbox.org>
2018-01-06 20:52:34 +0100

    add config.h + OTA flashing support

--- 1d-led-stripe.ino
+++ 1d-led-stripe.ino
@@ -1,38 +1,90 @@
 // NeoPixel Ring simple sketch (c) 2013 Shae Erisson
 // released under the GPLv3 license to match the rest of the AdaFruit NeoPixel library
 
+#include "config.h"
+#include <ArduinoOTA.h>
 #include <Adafruit_NeoPixel.h>
+
 #ifdef __AVR__
   #include <avr/power.h>
 #endif
-
-// Which pin on the Arduino is connected to the NeoPixels?
-// On a Trinket or Gemma we suggest changing this to 1
-#define LED_DATA_PIN   D2
-#define BUTTON_1_PIN   D6
-#define BUTTON_2_PIN   D7
-
-// How many NeoPixels are attached to the Arduino?
-#define NUMPIXELS      48
-const int range = 6;
 
 enum direction {
   UP,
   DOWN
 };
 
-static const int ledCount = NUMPIXELS;
-static int ledPos = NUMPIXELS / 2;
+static const int ledCount = NUM_LEDS;
+static int ledPos = NUM_LEDS / 2;
 static int direction = UP;
 
 // When we setup the NeoPixel library, we tell it how many pixels, and which pin to use to send signals.
 // Note that for older NeoPixel strips you might need to change the third parameter--see the strandtest
 // example for more information on possible values.
-Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, LED_DATA_PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUM_LEDS, LED_DATA_PIN, NEO_GRB + NEO_KHZ800);
 
 int delayval = 100; // delay for half a second
 
+void setupOta() {
+  Serial.println("Booting");
+  
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(wifi_ssid, wifi_password);
+  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.println("Connection Failed! Rebooting...");
+    delay(5000);
+    ESP.restart();
+  }
+  
+  // Port defaults to 8266
+  ArduinoOTA.setPort(8266);
+
+  // Hostname defaults to esp8266-[ChipID]
+  ArduinoOTA.setHostname(wifi_ota_name);
+
+  // No authentication by default
+  ArduinoOTA.setPassword(wifi_ota_password);
+
+  // Password can be set with it's md5 value as well
+  // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
+  // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
+
+  ArduinoOTA.onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH)
+      type = "sketch";
+    else // U_SPIFFS
+      type = "filesystem";
+
+    // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+    Serial.println("Start updating " + type);
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+  ArduinoOTA.begin();
+  Serial.println("Ready");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
 void setup() {
+  Serial.begin(115200);
+  Serial.println("bla\n");
+  
+  setupOta();  
+  
   // This is for Trinket 5V 16MHz, you can remove these three lines if you are not using a Trinket
 #if defined (__AVR_ATtiny85__)
   if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
@@ -44,19 +96,19 @@ void setup() {
   pinMode(BUTTON_1_PIN, INPUT_PULLUP);
   pinMode(BUTTON_2_PIN, INPUT_PULLUP);
 
-  Serial.begin(9600);
+  
 
   while (!Serial); // wait for serial port to connect. Needed for native USB port only  
 }
 
 void clearLeds() {    
-  for(int i = 0; i < NUMPIXELS; ++i) {
+  for(int i = 0; i < NUM_LEDS; ++i) {
     int red = 0;
     int green = 0;
     int blue = 0;     
 
     // Player 1
-    if(i >= (NUMPIXELS - range) || (i < range)) {
+    if(i >= (NUM_LEDS - RANGE) || (i < RANGE)) {
       red   = 0;
       green = 0;
       blue  = 16;
@@ -67,7 +119,7 @@ void clearLeds() {
 }
 
 void turnOffAllLeds() {
-  for(int i = 0; i < NUMPIXELS; i++)
+  for(int i = 0; i < NUM_LEDS; i++)
     pixels.setPixelColor(i, pixels.Color(0,0,0));
 
   pixels.show();
@@ -85,7 +137,7 @@ void die() {
     delay(500);
   }
 
-  ledPos = NUMPIXELS / 2;
+  ledPos = NUM_LEDS / 2;
   direction = direction == UP ? DOWN : UP;  
 }
 
@@ -93,17 +145,19 @@ void checkButtons() {
   int b1 = digitalRead(BUTTON_1_PIN);
   int b2 = digitalRead(BUTTON_2_PIN);
   
-  if(ledPos >= (NUMPIXELS - range)) { // Player 1
+  if(ledPos >= (NUM_LEDS - RANGE)) { // Player 1
     if(b1)
       direction = DOWN;  
   }
-  else if(ledPos < range) { // Player 2
+  else if(ledPos < RANGE) { // Player 2
     if(b2)
       direction = UP;  
   }
 }
 
 void loop() {    
+  ArduinoOTA.handle();
+  
   for(int i = 0; i < ledCount; i++){
     clearLeds();
     pixels.setPixelColor(ledPos, pixels.Color(0,150,0)); // Moderately bright green color.        
@@ -120,9 +174,10 @@ void loop() {
   if(ledPos == 0)
     die();
   
-  if(ledPos == NUMPIXELS - 1)
+  if(ledPos == NUM_LEDS - 1)
     die();    
 
   checkButtons();
 }
+
 
---
+++ config.h
@@ -0,0 +1,17 @@
+// ---------------------------------------------------------------
+// !WARNING! >> DO NOT CHECK IN LOG IN DATA INTO GIT! << !WARNING!
+// ---------------------------------------------------------------
+
+// WIFI:
+const char* wifi_ssid = <put ssid here>
+const char* wifi_password = <put wifi password here>
+const char* wifi_ota_name = <put OTA name here>
+const char* wifi_ota_password = <put OTA password here>
+
+// LED stripe:
+#define LED_DATA_PIN 4 // Data pin where LED stripe is connected on Node MCU
+#define NUM_LEDS 59    // Number of LEDs on the stripe
+#define BUTTON_1_PIN D6
+#define BUTTON_2_PIN D7
+#define RANGE 6
+
