a4cdb1858264d142d59dd3277614ee0d979b7b33
coon <coon@c-base.org>
2018-01-14 21:02:25 +0100

    config.h: add static modifiers + include guards

--- config.h
+++ config.h
@@ -2,11 +2,14 @@
 // !WARNING! >> DO NOT CHECK IN LOG IN DATA INTO GIT! << !WARNING!
 // ---------------------------------------------------------------
 
+#ifndef _CONFIG_H
+#define _CONFIG_H
+
 // WIFI:
-const char* wifi_ssid = <put ssid here>
-const char* wifi_password = <put wifi password here>
-const char* wifi_ota_name = <put OTA name here>
-const char* wifi_ota_password = <put OTA password here>
+static const char* wifi_ssid = <put ssid here>
+static const char* wifi_password = <put wifi password here>
+static const char* wifi_ota_name = <put OTA name here>
+static const char* wifi_ota_password = <put OTA password here>
 
 // LED stripe:
 #define LED_DATA_PIN D2 // Data pin where LED stripe is connected
@@ -14,3 +17,6 @@ const char* wifi_ota_password = <put OTA password here>
 #define BUTTON_1_PIN D7 // Button of player 1
 #define BUTTON_2_PIN D6 // Button of Player 2
 #define RANGE 6         // Number of base LEDs per player
+
+#endif //_CONFIG_H
+
