573bb0327f922f14b18fad8707307938298b2c76
coon <coon@c-base.org>
2018-01-14 21:13:45 +0100

    layout OTA logic to ota.h + ota.cpp

--- 1d-led-stripe.ino
+++ 1d-led-stripe.ino
@@ -1,70 +1,18 @@
-#include "config.h"
-#include <ArduinoOTA.h>
-#include <Adafruit_NeoPixel.h>
+#include "ota.h"
 #include "1dpong.h"
-
-void setupOta() {
-  Serial.println("Booting");
-
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(wifi_ssid, wifi_password);
-  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
-    Serial.println("Connection Failed! Rebooting...");
-    delay(5000);
-    ESP.restart();
-  }
-
-  ArduinoOTA.setPort(8266);
-  ArduinoOTA.setHostname(wifi_ota_name);
-  ArduinoOTA.setPassword(wifi_ota_password);
-
-  ArduinoOTA.onStart([]() {
-    String type;
-    if (ArduinoOTA.getCommand() == U_FLASH)
-      type = "sketch";
-    else // U_SPIFFS
-      type = "filesystem";
-
-    // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
-    Serial.println("Start updating " + type);
-  });
-
-  ArduinoOTA.onEnd([]() {
-    Serial.println("\nEnd");
-  });
-
-  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-  });
-
-  ArduinoOTA.onError([](ota_error_t error) {
-    Serial.printf("Error[%u]: ", error);
-    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
-    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
-    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
-    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
-    else if (error == OTA_END_ERROR) Serial.println("End Failed");
-  });
-
-  ArduinoOTA.begin();
-  Serial.println("Ready");
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
-}
 
 OneDimensionalPong _pongGame;
 
 void setup() {
   Serial.begin(115200);
+  while (!Serial); // wait for serial port to connect. Needed for native USB port only
+  
   setupOta();
   _pongGame.init();
-
-  while (!Serial); // wait for serial port to connect. Needed for native USB port only
 }
 
 void loop() {
-  ArduinoOTA.handle();
-  
+  ArduinoOTA.handle();  
   _pongGame.tick();
 }
 
---
+++ ota.h
@@ -0,0 +1,8 @@
+#ifndef _OTA_H
+#define _OTA_H
+
+#include <ArduinoOTA.h>
+
+void setupOta();
+
+#endif // !_OTA_H
